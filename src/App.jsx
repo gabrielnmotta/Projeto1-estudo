78635baa8e0fb929c8fe9dede017f2e2b29d2504
Gabriel Nogueira <gabrieln500@gmail.com>
2022-12-28 13:42:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+  import {Post} from './Post'
+
+export function App() {
+
+  return (
+   <>
+    <Post author="Gabriel" Content="someting"/>
+    <Post author="Nogueira" Content="something"/>
+   </> 
+  )
+}
+
+
